cf53e8875790439555490f3bea2e875ba45cbd2a
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-12 00:29:05 +0000

    Added proper Hello, World! format

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Created by: Ben Lapuhapo
+#Created on: September 11 2019
+#This is the "Hello, World" Program, But with the proper style
+
+def main():
+    print("Hello, World!")
+    
+    
+    
+if __name__ == "__main__":
+    main()
